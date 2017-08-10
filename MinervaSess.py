c61a3cfb0ea590e229a3dd55009b5c9b6d3ad317
JRegimbal <juliette.regimbal@gmail.com>
2017-08-09 20:51:55 -0400

    adding functionality

--- MinervaSess.py
+++ MinervaSess.py
@@ -1,7 +1,8 @@
 import requests
 
 urlBase = 'https://horizon.mcgill.ca'
-urls = {'login':'/pban1/twbkwbis.P_ValLogin'}
+urls = {'login':'/pban1/twbkwbis.P_ValLogin',
+        'term' : '/pban1/bwckgens.p_proc_term_date'}
 
 class MinervaSess:
     def __init__(self, sid, PIN):
@@ -9,13 +10,43 @@ class MinervaSess:
         #self.sid = sid
         #self.PIN = PIN
         self.resp = None
-        self.cookies = {'TESTID' : 'set'}
 
     def login(self):
         if self.resp != None:
             return True
-        self.resp = requests.post(urlBase+urls['login'], data=self.payload, cookies=self.cookies)
-        if noerror: #this but real
-            return True
-        self.resp = None #clear this since it didn't work
-        return False
+        self.resp = requests.post(urlBase+urls['login'], data=self.payload, cookies={'TESTID' : 'set'})
+        #start checking success
+        try:
+            self.resp.cookies['SESSID']
+        except KeyError:
+            self.resp = None #clear it
+            return False
+        self.jar = self.resp.cookies.get_dict()
+        return True
+
+    def setTerm(self, session, year):
+        if self.resp == None:
+            return False
+        if session == 'fall':
+            session = 'Fall'
+        elif session == 'win':
+            session = 'Winter'
+        else:
+            session = 'Summer'
+        old_sessid = self.jar['SESSID']
+        self.resp = requests.post(urlBase+urls['term'], data={'p_term':session+' '+year}, cookies=self.jar)
+
+        if old_sessid == self.resp.cookies['SESSID']:
+            return False
+        self.jar = self.resp.cookies.get_dict()
+        return True
+
+    def searchCRN(self, crn, prefix, faculty):
+        if self.resp == None:
+            return -1
+        old_sesid = self.jar['SESSID']
+        data = {'crn':crn, 'sel_subj':prefix, 'sel_coll':faculty}
+        self.resp = requests.post(urlBase+urls['search'], data=data, cookies=self.jar)
+        if old_sessid == self.resp.cookies['SESSID']:
+            return -1
+        
